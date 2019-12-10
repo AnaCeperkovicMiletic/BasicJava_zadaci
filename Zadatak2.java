1a4779dfc6bd65ed8b1136109bedb93c95b16216
AnaCeperkovicMiletic <58726954+AnaCeperkovicMiletic@users.noreply.github.com>
2019-12-10 10:46:49 +0100

    Domaci 1, zadatak 2
    Kalkulator

---
+++ Zadatak2.java
@@ -0,0 +1,40 @@
+package homework1;
+
+import java.util.Scanner;
+
+public class Zadatak2 {
+
+	public static void main(String[] args) {
+		int a;
+		int b;
+		String operacija = "";
+
+		Scanner ulaz = new Scanner(System.in);
+		System.out.println("Unesite dva cela broja.");
+
+		a = ulaz.nextInt();
+		b = ulaz.nextInt();
+
+		System.out.println("Unesite operaciju (‘s’ – sabiranje, ‘o’ – oduzimanje, ‘m’ – množenje, ‘d’ – deljenje, ‘e’ - izlaz).");
+
+		operacija = ulaz.next();
+
+		double rezultat = 0;
+
+		switch (operacija) {
+		case "s":
+			rezultat = a + b;
+			break;
+		case "o":
+			rezultat = a - b;
+			break;
+		case "m":
+			rezultat = a * b;
+			break;
+		case "d":
+			rezultat = a / b;
+			break;
+		}
+		System.out.println("Rezultat je: " + rezultat);
+	}
+}
