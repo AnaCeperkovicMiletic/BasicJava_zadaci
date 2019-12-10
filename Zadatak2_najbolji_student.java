242f5ca26cf026155628337b7f44e90fa2da3803
AnaCeperkovicMiletic <58726954+AnaCeperkovicMiletic@users.noreply.github.com>
2019-12-10 16:49:37 +0100

    Update Zadatak2_najbolji_student.java

--- Zadatak2_najbolji_student.java
+++ Zadatak2_najbolji_student.java
@@ -11,22 +11,22 @@ public class Zadatak2_najbolji_student {
 
 		int brojStudenata = ulaz.nextInt();
 		String[] imenaStudenata = new String[brojStudenata];
-		int[] ocene = new int[brojStudenata];
+		int[] brojPoena = new int[brojStudenata];
 
 		for (int i = 0; i < brojStudenata; i++) {
 			System.out.print("Uneti ime i ocenu studenta: ");
 
 			imenaStudenata[i] = ulaz.next();
-			ocene[i] = ulaz.nextInt();
+			brojPoena[i] = ulaz.nextInt();
 		}
 		int max = 0;
 		for (int i = 0; i < brojStudenata; i++) {
-			if (ocene[i] > max) max = ocene[i];
+			if (brojPoena[i] > max) max = brojPoena[i];
 		}
 		
 		System.out.print("Studenti sa najvisim brojem poena na testu su: ");
 		for (int i = 0; i < brojStudenata; i++) {
-			if (max == ocene[i]) {
+			if (max == brojPoena[i]) {
 				System.out.print(imenaStudenata[i] + " ");
 				
 			}
