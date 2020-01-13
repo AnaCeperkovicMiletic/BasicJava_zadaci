607b1522d1000af30c4c1622f9da423441b49a61
AnaCeperkovicMiletic <58726954+AnaCeperkovicMiletic@users.noreply.github.com>
2020-01-13 12:12:56 +0100

    Domaci 5_2 import studenata

---
+++ Student.java
@@ -0,0 +1,43 @@
+package homework5_2;
+
+public class Student {
+	
+	String ime;
+	String prezime;
+	int brojPoena;
+	int ocena = 0;
+	
+	public Student(String ime, String prezime, int brojPoena) throws Exception {
+		
+		if (brojPoena < 0 || brojPoena > 100) {
+			throw new Exception("Broj poena mora biti od 0 do 100.");
+		}
+		this.ime = ime;
+		this.prezime = prezime;
+		this.brojPoena = brojPoena;
+		
+		if (brojPoena > 90) {
+			ocena = 10;
+		}
+		else if (80 < brojPoena && brojPoena <= 90) {
+			ocena = 9;
+		}
+		else if (70 < brojPoena && brojPoena <= 80) {
+			ocena = 8;
+		}
+		else if (60 < brojPoena && brojPoena <= 70) {
+			ocena = 7;
+		}
+		else if (50 < brojPoena && brojPoena <= 60) {
+			ocena = 6;
+		}
+		else if (brojPoena <= 50) {
+			ocena = 5;
+		}
+	}
+	
+	public String toString() {
+		return "Ime i prezime studenta: " + ime + " " + prezime + " Broj poena: " + brojPoena + " Ocena: " + ocena;
+	}
+	
+}
